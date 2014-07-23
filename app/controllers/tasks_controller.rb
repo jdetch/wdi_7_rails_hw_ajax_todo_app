976f564c5d08a42f0a2e9fc28499f2e532be7893
Jodi Detch <jodidetch@gmail.com>
2014-07-22 21:13:43 -0400

    App has a tasks controller and task resources

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+
+  # ONLY respond to request for JSON
+  respond_to :json
+
+  def index
+    @tasks = Task.all
+    # like a render json: @tasks
+    respond_with(@tasks)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    respond_with(@task)
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      respond_with(@task)
+    else respond_with(@task.errors)
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task)
+  end
+
+end
